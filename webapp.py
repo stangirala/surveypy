505c943510e750422368097b9d6d36fd416f9865
Sarma Tangirala <tvssarma.omega9@gmail.com>
2013-04-13 18:19:23 -0400

    Stupid App Ready

--- webapp.py
+++ webapp.py
@@ -1,7 +1,17 @@
-for flask import Flask
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route('/')
 def unknown():
   return "Well, this is embarrring. You don't follow instructions."
 
-@app.route('/push')
+@app.route('/pushsurvey', methods=['POST'])
+def pushsurvey():
+  if request.method == 'POST':
+    return request.json
+  else:
+    return abort(405)
+
+if __name__ == '__main__':
+  app.run()
